1c90f9f2257ee2cf92e7e8e852a778b9da306013
Jacob-Yangman <jacob7154@qq.com>
2021-09-21 11:04:39 +0800

    3rd commit -- change port

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# chatroom-v1
-An online groupchat room based on UDP server and client.
+A lightweight groupchat program ensures that users structure a multiplayer online chatroom.
+
+This program **includes** 2 main python scripts. A server and client.
+
+#Environment
+This program can be execute on 
+
+#Usage
+After adding 2 python files to proper directory, you need to configure chatroom-server first.
+
+To debug this program, the **server address** can be set to "0.0.0.0"(default value), you can choose any proper port as well. Besides, the address configuration of chatroom-client is consistent with that of the server, especially the port.
+
+When you finish testing and deploy the project to a server, change the address to public IP address in your server first.
+
+Execute chatroom-server first. You can announce any group notice any time.
+
+chatroom-client can run on any client PC. 
--- chatroom-server.py
+++ chatroom-server.py
@@ -11,10 +11,10 @@ from multiprocessing import Process
 
 # Address of Server
 HOST = "0.0.0.0"
-PORT = 7154
+PORT = 7500
 ADDR = (HOST, PORT)
 
-
+# A dict stored addresses and usernames of all connecting clients
 user = {}
 
 
@@ -43,7 +43,7 @@ def chat(sock, addr, message):
 
 def exit_group(sock, content):
     name = content.split(" ", 1)[0]
-    # 确保用户在列表才能删除
+    # delete user info if exists.
     if name in user:
         del user[name]
     for a in user.values():
@@ -51,37 +51,32 @@ def exit_group(sock, content):
 
 
 def handle(sock):
-    # 一处总体接收各种请求，分情况解析讨论
     while True:
         request, addr = sock.recvfrom(1024)
-        # print(request)
         info = request.decode().split(" ", 1)
         request = info[0]
         content = info[1]
-        # 分情况讨论
         if request == "LOGIN":
             join_group(sock, addr, content)
-            # print(DICT_USER)
         elif request == "CHAT":
             chat(sock, addr, content)
         elif request == "EXIT":
             exit_group(sock, content)
 
 
-# 先写入口　搭建框架
+# Entry function
 def main():
-    # 创建UDP网络模型
     sock = socket(type=SOCK_DGRAM)
     sock.bind(ADDR)
-    # 创建进程
+    # Create a process to receive requests from clients.
     p = Process(target=handle, args=(sock,), daemon=True)
     p.start()
-    # 创建管理员消息
+    # To create a group notice anytime you want.
     while True:
         admin_notice = input("Group Notice:")
-        # 通过进程间通信将管理员消息传入子进程
+        # Send group notice by child process.
         notice = "CHAT " + "Group Notice:" + admin_notice
-        sock.sendto(notice.encode(), ADDR)      # 注意这里是发给服务端
+        sock.sendto(notice.encode(), ADDR)
 
 
 if __name__ == '__main__':
